1f1843cb67176b45f7ae2b918df7bd3b215f22b2
imsk17 <imsumitkr17@gmail.com>
2023-12-02 15:43:23 +0530

    Day1: Part2: Nasty Solution

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -24,3 +24,39 @@ pub mod part1 {
         return acc;
     }
 }
+
+pub mod part2 {
+    const INPUT: &str = include_str!("../../inputs/day1/part1.txt");
+
+    pub fn solution() -> u32 {
+        let mut acc = 0;
+        let sanitized = INPUT
+            .replace("one", "o1ne")
+            .replace("two", "t2wo")
+            .replace("three", "th3ree")
+            .replace("four", "fo4ur")
+            .replace("five", "fi5ve")
+            .replace("six", "s6ix")
+            .replace("seven", "sev7en")
+            .replace("eight", "ei8ght")
+            .replace("nine", "ni9ne");
+
+        for line in sanitized.lines() {
+            let mut digits_in_line = line.chars().filter(|c| c.is_digit(10));
+            let [first, .., last] = if digits_in_line.clone().count() < 2 {
+                [
+                    digits_in_line.clone().next().unwrap(),
+                    digits_in_line.clone().next().unwrap(),
+                ]
+            } else {
+                [
+                    digits_in_line.next().unwrap(),
+                    digits_in_line.last().unwrap_or('0'),
+                ]
+            };
+            let digit = format!("{}{}", first, last).parse::<u32>().unwrap();
+            acc += digit;
+        }
+        return acc;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,7 @@ mod day1;
 
 fn main() {
     let day1_part1 = day1::part1::solution();
+    let day1_part2 = day1::part2::solution();
     println!("Day 1, part 1: {}", day1_part1);
+    println!("Day 1, part 2: {}", day1_part2);
 }
