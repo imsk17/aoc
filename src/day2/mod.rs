6f1a5e899e06e02fdfee00220a8f79cc1a3087c3
imsk17 <imsumitkr17@gmail.com>
2023-12-03 02:00:20 +0530

    Day2: Part2: Another Very Functional Solution God bless rust iterators

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -39,3 +39,54 @@ pub mod part1 {
         result
     }
 }
+
+pub mod part2 {
+
+    const INPUT: &str = include_str!("../../inputs/day2/part1.txt");
+
+    pub fn solution() -> u32 {
+        let result = INPUT
+            .lines()
+            .map(|e| {
+                e.split_once(": ")
+                    .unwrap()
+                    .1
+                    .split(";")
+                    .map(|s| s.split(", ").map(|s| s.trim_start()).collect::<Vec<_>>())
+                    .collect::<Vec<_>>()
+            })
+            .map(|data| {
+                let result = data
+                    .iter()
+                    .flatten()
+                    .map(|e| e.split_once(" ").unwrap())
+                    .collect::<Vec<_>>();
+
+                let min_green = result
+                    .iter()
+                    .filter(|(_, color)| *color == "green")
+                    .map(|(count, _)| {
+                        let parsed = count.parse::<u32>().unwrap();
+                        return parsed;
+                    })
+                    .max()
+                    .unwrap_or(0);
+                let min_blue = result
+                    .iter()
+                    .filter(|(_, color)| *color == "blue")
+                    .map(|(count, _)| count.parse::<u32>().unwrap())
+                    .max()
+                    .unwrap_or(0);
+                let min_red = result
+                    .iter()
+                    .filter(|(_, color)| *color == "red")
+                    .map(|(count, _)| count.parse::<u32>().unwrap())
+                    .max()
+                    .unwrap_or(0);
+
+                min_blue * min_green * min_red
+            })
+            .sum();
+        result
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ fn main() {
     let day1_part2 = day1::part2::solution();
 
     let day2_part1 = day2::part1::solution();
+    let day2_part2 = day2::part2::solution();
     println!("Day 1, part 1: {}", day1_part1);
     println!("Day 1, part 2: {}", day1_part2);
     println!("Day 2, part 1: {}", day2_part1);
+    println!("Day 2, part 2: {}", day2_part2);
 }
